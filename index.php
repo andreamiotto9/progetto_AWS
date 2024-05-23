d001f338dd45638d964d396b29d6d1808cafcec3
andreamiotto9 <167292473+andreamiotto9@users.noreply.github.com>
2024-05-23 13:39:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,56 +1,18 @@
 <?php
-session_start();
 
-$servername = "172.18.0.3";
-$username = "andrea";
-$password = "password";
-$dbname = "your_docker_project";
-$conn = new mysqli($servername, $username, $password, $dbname);
+$host = 'localhost';
+$dbname = 'anagrafica';
+$dbuser = 'root';
+$dbpass = '';
+$dbport = 3306;
 
+$conn = new mysqli($host,$dbuser,$dbpass,$dbname,$dbport);
+//i dati devono essere scritti in questo ordine
+if($conn -> error){
+    echo 'errore di connessione';
 
-if ($conn->connect_error) {
-    die("Connessione al database fallita: " . $conn->connect_error);
+}else{
+    header("Location: login.php");
+    
 }
-
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-
-    $sql = "SELECT * FROM utente WHERE username=? AND password=?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ss", $username, $password);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    if ($result->num_rows == 1) {
-
-        $_SESSION["username"] = $username;
-        $_SESSION["password"] = $password;
-        header("Location: login.php");
-    } else {
-        header("Location: index.php");
-    }
-}
-
-$conn->close();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-<body>
-    <h2>Login</h2>
-    <form method="POST">
-        <label for="username">Username:</label><br>
-        <input type="text" id="username" name="username"><br>
-        <label for="password">Password:</label><br>
-        <input type="password" id="password" name="password"><br><br>
-        <input type="submit" value="Login">
-    </form>
-</body>
-</html>
