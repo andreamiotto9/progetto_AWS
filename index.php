4fec6eac2e2c686bde4050fc6399ce19d0bb28b4
andreamiotto9 <167292473+andreamiotto9@users.noreply.github.com>
2024-05-23 13:40:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
 
-$host = 'localhost';
-$dbname = 'anagrafica';
-$dbuser = 'root';
-$dbpass = '';
-$dbport = 3306;
+$servername = "172.18.0.3";
+$username = "andrea";
+$password = "password";
+$dbname = "your_docker_project";
 
-$conn = new mysqli($host,$dbuser,$dbpass,$dbname,$dbport);
+$conn = new mysqli($servername,$username,$password,$dbname);
 //i dati devono essere scritti in questo ordine
 if($conn -> error){
     echo 'errore di connessione';
